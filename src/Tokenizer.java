6bc8ab5ab1ad945024d82e8d7f18df2ed21e70cd
nomon <thanaporn_chan@cmu.ac.th>
2023-02-15 10:45:30 +0700

    edit tokenizer

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -3,5 +3,7 @@ public interface Tokenizer
     boolean hasNextToken();
     String peek();
     String consume() throws LexicalError;
+    boolean peek(String s);
+    void consume(String s) throws SyntaxError, LexicalError;
 }
 
